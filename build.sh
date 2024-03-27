3ee1111a08d0c0685d44869a58b686b4e18dee9b
Il Harper <hi@ilharper.com>
2024-03-28 01:36:12 +0800

    ci: fix CI

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ set -euxo pipefail
 mkdir -p protobuf/cmake/build
 cd protobuf/cmake/build
 
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_STANDARD=23 -DCMAKE_CXX_STANDARD=23 -Dprotobuf_BUILD_TESTS=OFF -DABSL_BUILD_TESTING=OFF -DABSL_USE_GOOGLETEST_HEAD=OFF -DABSL_PROPAGATE_CXX_STD=ON -Dprotobuf_WITH_ZLIB=OFF -DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE -DCMAKE_INSTALL_PREFIX=. ${LIBPROTOBUF_PREBUILT_GENERATE_ARG} ../..
-cmake --build . --config Release ${LIBPROTOBUF_PREBUILT_BUILD_ARG}
+readarray -t -d '' generate_args < <(xargs printf '%s\0' <<<"${LIBPROTOBUF_PREBUILT_GENERATE_ARGS}")
+readarray -t -d '' build_args < <(xargs printf '%s\0' <<<"${LIBPROTOBUF_PREBUILT_BUILD_ARGS}")
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_STANDARD=23 -DCMAKE_CXX_STANDARD=23 -Dprotobuf_BUILD_TESTS=OFF -DABSL_BUILD_TESTING=OFF -DABSL_USE_GOOGLETEST_HEAD=OFF -DABSL_PROPAGATE_CXX_STD=ON -Dprotobuf_WITH_ZLIB=OFF -DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE -DCMAKE_INSTALL_PREFIX=. "${generate_args[@]}" ../..
+cmake --build . --config Release "${build_args[@]}"
 cmake --install . --config Release
 
 cd ../../..
